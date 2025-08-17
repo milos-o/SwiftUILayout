1f19350da914c8bec878858e9f3d7dbb7c56d3f8
Milos Ostojic <milos.osto11@gmail.com>
2025-08-17 12:05:15 +0200

    Adds logic for calculating views size and frame

--- SwiftUILayout/SwiftUILayout/ContentView.swift
+++ SwiftUILayout/SwiftUILayout/ContentView.swift
@@ -17,7 +17,8 @@ typealias RenderingContext = CGContext
 typealias ProposedSize = CGSize
 
 protocol BuiltinView {
-    func render(context: RenderingContext, size: ProposedSize)
+    func render(context: RenderingContext, size: CGSize)
+    func size(proposed: ProposedSize) -> CGSize
     typealias Body = Never
 }
 
@@ -32,11 +33,19 @@ extension Never: View_ {
 }
 
 extension View_ {
-    func _render(context: RenderingContext, size: ProposedSize) {
+    func _render(context: RenderingContext, size: CGSize) {
         if let builtin = self as? BuiltinView {
             builtin.render(context: context, size: size)
         } else {
             body._render(context: context, size: size)
+        }
+    }
+    
+    func _size(proposed: ProposedSize) -> CGSize {
+        if let builtin = self as? BuiltinView {
+            return builtin.size(proposed: proposed)
+        } else {
+            return body._size(proposed: proposed)
         }
     }
 }
@@ -61,6 +70,10 @@ struct ShapeView<S: Shape_>: BuiltinView, View_ {
     var shape: S
     var color: NSColor = .red
     
+    func size(proposed: ProposedSize) -> CGSize {
+        return proposed
+    }
+    
     func render(context: RenderingContext, size: ProposedSize) {
         context.saveGState()
         context.setFillColor(color.cgColor)
@@ -82,12 +95,43 @@ struct Ellipse_: Shape_ {
     }
 }
 
-let sample = NSColor.blue
+struct FixedFrame<Content: View_>: View_, BuiltinView {
+    var width: CGFloat?
+    var height: CGFloat?
+    var content: Content
+    
+    func size(proposed: ProposedSize) -> CGSize {
+        let childSize = content._size(proposed: ProposedSize(width: width ?? proposed.width, height: height ?? proposed.height))
+        return CGSize(width: width ?? childSize.width, height: height ?? childSize.height)
+    }
+    
+    func render(context: RenderingContext, size: ProposedSize) {
+        context.saveGState()
+        let childSize = content._size(proposed: size)
+        let x = (size.width - childSize.width) / 2
+        let y = (size.height - childSize.height) / 2
+        context.translateBy(x: x, y: y)
+        content._render(context: context, size: childSize)
+        context.restoreGState()
+    }
+}
+
+extension View_ {
+    func frame(width: CGFloat? = nil, height: CGFloat? = nil) -> some View_ {
+        FixedFrame(width: width, height: height, content: self)
+    }
+}
+
+var sample: some View_ {
+    NSColor.blue.frame(width: 200, height: 100)
+}
 
 func render<V: View_>(view: V) -> Data {
     let size = CGSize(width: 600, height: 400)
     return CGContext.pdf(size: size) { context in
-        view._render(context: context, size: size)
+        view
+            .frame(width: size.width, height: size.height)
+            ._render(context: context, size: size)
     }
 }
 
