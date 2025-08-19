0dd3a972c80105223cf53d36823550915cf9d782
Milos Ostojic <milos.osto11@gmail.com>
2025-08-19 15:37:23 +0200

    Adds Text_ implementation

---
+++ SwiftUILayout/SwiftUILayout/NotSwiftUI/Text.swift
@@ -0,0 +1,45 @@
+//
+//  Text.swift
+//  SwiftUILayout
+//
+//  Created by Milos on 19. 8. 2025..
+//
+
+import SwiftUI
+
+struct Text_: View_, BuiltinView {
+    let text: String
+    
+    init(_ text: String) {
+        self.text = text
+    }
+    
+    let font = NSFont.systemFont(ofSize: 16)
+    var attributes: [NSAttributedString.Key: Any] {
+        [
+            .font: font,
+            .foregroundColor: NSColor.white
+        ]
+    }
+    
+    var framesetter: CTFramesetter {
+        let str = NSAttributedString(string: text, attributes: attributes)
+        return CTFramesetterCreateWithAttributedString(str)
+    }
+    
+    func render(context: RenderingContext, size: CGSize) {
+        let path = CGPath(rect: CGRect(origin: .zero, size: size), transform: nil)
+        let frame = CTFramesetterCreateFrame(framesetter, CFRange(), path, nil)
+        context.saveGState()
+        CTFrameDraw(frame, context)
+        context.restoreGState()
+    }
+    
+    func size(proposed: ProposedSize) -> CGSize {
+       return CTFramesetterSuggestFrameSizeWithConstraints(framesetter, CFRange(), nil, proposed, nil)
+    }
+    
+    var swiftUI: some View {
+        Text(text).font(Font(font))
+    }
+}
